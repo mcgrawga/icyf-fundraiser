4b03a3c31d8eb053f84f30fffb0852574bb3673f
mcgrawga <garthmcgraw@statgolf.com>
2017-07-10 21:26:02 -0500

    Tyring to fix password reset email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,22 @@ Ncaa::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.playoffpoolz.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['SUPPORT_PLAYOFFPOOLZ_EMAIL'],
+    password: ENV['SUPPORT_PLAYOFFPOOLZ_EMAIL_PASSWORD']
+  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
